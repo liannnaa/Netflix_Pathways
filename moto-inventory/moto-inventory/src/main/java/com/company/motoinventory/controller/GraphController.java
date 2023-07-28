471a78cc208b6bf8bfb61a8cadcd5cd98f9b2732
liannnaa <lnp2016@nyu.edu>
2023-07-27 19:18:33 -0500

    added mutation mapping

--- moto-inventory/moto-inventory/src/main/java/com/company/motoinventory/controller/GraphController.java
+++ moto-inventory/moto-inventory/src/main/java/com/company/motoinventory/controller/GraphController.java
@@ -4,6 +4,7 @@ import com.company.motoinventory.models.Motorcycle;
 import com.company.motoinventory.repository.MotorcycleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -22,5 +23,29 @@ public class GraphController {
     @QueryMapping
     public Motorcycle findMotorcycleById(@Argument String id){
         return motorcycleRepository.getMotorcycleById(id);
+    }
+
+    @MutationMapping
+    public Motorcycle addMotorcycle(
+            @Argument String id,
+            @Argument String make,
+            @Argument String model,
+            @Argument int year) {
+        return motorcycleRepository.addMotorcycle(id, make, model, year);
+    }
+
+    @MutationMapping
+    public Motorcycle updateMotorcycle(
+            @Argument String id,
+            @Argument String make,
+            @Argument String model,
+            @Argument int year) {
+        Motorcycle updatedMotorcycle = new Motorcycle(id, make, model, year);
+        return motorcycleRepository.updateMotorcycle(updatedMotorcycle);
+    }
+
+    @MutationMapping
+    public boolean deleteMotorcycleById(@Argument String id) {
+        return motorcycleRepository.deleteMotorcycleById(id);
     }
 }
